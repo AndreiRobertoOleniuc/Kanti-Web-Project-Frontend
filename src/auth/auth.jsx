59b174fa07c79f5726c4276336ee3b208ed83809
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2020-10-07 23:43:50 +0200

    check if eingabe not null

--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -1,7 +1,11 @@
 const auth = {
   isAuthenticated: false,
   authenticate(cb, name, vorname) {
-    auth.isAuthenticated = true;
+    if (name.length < 1 || vorname.length < 1) {
+      auth.isAuthenticated = false;
+    } else {
+      auth.isAuthenticated = true;
+    }
     setTimeout(cb, 100);
   },
   signout(cb) {
